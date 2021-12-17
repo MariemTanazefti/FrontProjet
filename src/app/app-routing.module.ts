a7eddbf574c81d74d9cd6113d7736219df95ea1a
MARIEM <tanazeftimariem9@gmail.com>
2021-12-17 16:18:59 +0100

    FirstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component,NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { BacklogComponent } from './backlog/backlog.component';
+import { GitComponent } from './git/git.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { PersonnesComponent } from './personnes/personnes.component';
+import { ProjetsComponent } from './projets/projets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'projets',component:ProjetsComponent},
+  {path:'personnes',component:PersonnesComponent},
+  {path:'backlog',component:BacklogComponent},
+  {path:'git',component:GitComponent},
+  {path:'kanban',component:KanbanComponent},
+  {path:'auth',component:AuthComponent},
+  { path: '',   redirectTo: '/auth', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { PersonnesComponent } from './personnes/personnes.component';
+import { BacklogComponent } from './backlog/backlog.component';
+import { GitComponent } from './git/git.component';
+import { AuthComponent } from './auth/auth.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjetsComponent,
+    PersonnesComponent,
+    BacklogComponent,
+    GitComponent,
+    AuthComponent,
+    KanbanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
